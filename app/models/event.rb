c67ac855941e817abef1e3490093a30af3926442
williamprieto <willopv@gmail.com>
2016-08-04 16:35:00 -0500

    validacion sin mostrar mensajes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,12 @@
 class Event < ApplicationRecord
-  validates :responsable, :nombreEven, :lugar, :fechaHora, :descripcion, :telefono, :email,:presence => {:message => "Espacios vacios"}
+  validates :responsable, presence:true, length: { maximum: 5 }
+  validates :nombreEven, presence:true
+  validates :lugar, presence:true
+  validates :fechaHora, presence:true
+  #validates :descripcion, presence:true
+  validates :telefono, presence:true, numericality: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: true
 end
