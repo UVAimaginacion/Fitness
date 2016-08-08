f052cacd73fd8b1c00bc90e4002dafdc8b6f16d9
williamprieto <willopv@gmail.com>
2016-08-08 16:45:50 -0500

    formulario y validacion

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,7 +10,9 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
   end
-
+  def edit
+    @event = Event.find(params[:id])
+  end
   def create
     @event = Event.new(event_params)
 
@@ -19,6 +21,21 @@ class EventsController < ApplicationController
     else
       render 'new'
     end
+  end
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render 'edit'
+    end
+  end
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+
+    redirect_to events_path
   end
 
   private
