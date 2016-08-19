86b635b1267c34419e3d72ebd3da49158cf68e8c
williamprieto <willopv@gmail.com>
2016-08-18 20:00:10 -0500

    cancan sin modificar vistas sin asociaciones

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  enum rol: [ :admin, :usuario  ]
+
+  before_create :define_rol
+
+  def define_rol
+    self.rol = "usuario" unless self.rol.present?
+  end
+
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,6 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "m@gmail.com",  password: "123456789", rol: "admin" )
+User.create(email: "admin@gmail.com", password: "123456789", rol: "admin" )
 
